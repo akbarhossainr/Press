36379ce81e37312f84cf0e23c59c9ae4363c2bb1
akbarhossainr <akbarhossain15@gmail.com>
2021-06-25 00:23:14 +0600

    InitialTest script added

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace akbahossair\Press\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
